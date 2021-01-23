842df25cc08eeaede3e3762ea11f21967a144a9f
Jamie <Jamie.burge@students.plymouth.ac.uk>
2021-01-23 13:24:15 +0000

    Testing a new way of pushing

--- Comp1004 Mount Edgecumbe/wwwroot/js/SPA.js
+++ Comp1004 Mount Edgecumbe/wwwroot/js/SPA.js
@@ -5,4 +5,14 @@
     $('#home').click(function () {
         $('#page-content-wrapper').load('IndexPanel.html');
     })
+    $('#ig').click(function () {
+        $('#page-content-wrapper').load('ig.html');
+    })
+    $('#gjg').click(function () {
+        $('#page-content-wrapper').load('GJG.html');
+    })
+    $('#bba').click(function () {
+        $('#page-content-wrapper').load('BBA.html');
+    })
+
 });
