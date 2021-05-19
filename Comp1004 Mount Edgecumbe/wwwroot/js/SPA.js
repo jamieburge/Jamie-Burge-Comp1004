f1623b95f1d5f1a225f6a0c13fa2c06595024ea8
Jamie <Jamie.burge@students.plymouth.ac.uk>
2021-05-19 14:07:37 +0100

    I forgot to push my last couple of sprints so this is sprints 4 5 and 6

--- Comp1004 Mount Edgecumbe/wwwroot/js/SPA.js
+++ Comp1004 Mount Edgecumbe/wwwroot/js/SPA.js
@@ -1,9 +1,4 @@
-﻿
-//Adding a tile layer (the background image map) to our Map
-//We use the addTo method to add objects to our Map
-
-
-$(document).ready(function () {
+﻿$(document).ready(function () {
     $(".dropdown-toggle").dropdown();
 });
 $(document).ready(function () {
@@ -26,49 +21,49 @@ $(document).ready(function () {
         $('#page-content-wrapper').load('bba.html');
     })
     $('#FG').click(function () {
-        $('#page-content-wrapper').load('French Garden.html');
+        $('#page-content-wrapper').load('French-Garden.html');
     })
-    $('#Relic Garden').click(function () {
-        $('#page-content-wrapper').load('Relic Garden.html');
+    $('#Relic-Garden').click(function () {
+        $('#page-content-wrapper').load('Relic-Garden.html');
     })
-    $('#New Trees').click(function () {
-        $('#page-content-wrapper').load('New Trees.html');
+    $('#New-Trees').click(function () {
+        $('#page-content-wrapper').load('New-Trees.html');
     })
     $('#Camellias').click(function () {
         $('#page-content-wrapper').load('Camellias.html');
     })
-    $('#Diamond Jubilee Copse').click(function () {
-        $('#page-content-wrapper').load('Diamond Jubilee Copse.html');
+    $('#Diamond-Jubilee-Copse').click(function () {
+        $('#page-content-wrapper').load('Diamond-Jubilee-Copse.html');
     })
-    $('#Friends Glade').click(function () {
-        $('#page-content-wrapper').load('Friends Glade.html');
+    $('#Friends-Glade').click(function () {
+        $('#page-content-wrapper').load('Friends-Glade.html');
     })
-    $('#Barrow Centre').click(function () {
-        $('#page-content-wrapper').load('Barrow Centre.html');
+    $('#Barrow-Centre').click(function () {
+        $('#page-content-wrapper').load('Barrow-Centre.html');
     })
     $('#Batcam').click(function () {
         $('#page-content-wrapper').load('Batcam.html');
     })
-    $('#Fire Auxiliary').click(function () {
-        $('#page-content-wrapper').load('Fire Auxiliary.html');
+    $('#Fire-Auxiliary').click(function () {
+        $('#page-content-wrapper').load('Fire-Auxiliary.html');
     })
-    $('#Deer Garden').click(function () {
-        $('#page-content-wrapper').load('Deer Garden.html');
+    $('#Deer-Garden').click(function () {
+        $('#page-content-wrapper').load('Deer-Garden.html');
     })
-    $('#Knot Garden').click(function () {
-        $('#page-content-wrapper').load('Knot Garden.html');
+    $('#Knot-Garden').click(function () {
+        $('#page-content-wrapper').load('Knot-Garden.html');
     })
-    $('#Cedar Seat').click(function () {
-        $('#page-content-wrapper').load('Cedar Seat.html');
+    $('#Cedar-Seat').click(function () {
+        $('#page-content-wrapper').load('Cedar-Seat.html');
     })
-    $('#Shell Seat').click(function () {
-        $('#page-content-wrapper').load('Shell Seat.html');
+    $('#Shell-Seat').click(function () {
+        $('#page-content-wrapper').load('Shell-Seat.html');
     })
-    $('#Lambert Lodge').click(function () {
-        $('#page-content-wrapper').load('Lambert Lodge.html');
+    $('#Lambert-Lodge').click(function () {
+        $('#page-content-wrapper').load('Lambert-Lodge.html');
     })
-    $('#Childrens Play Area').click(function () {
-        $('#page-content-wrapper').load('Childrens Play Area.html');
+    $('#Childrens-Play-Area').click(function () {
+        $('#page-content-wrapper').load('Childrens-play-area.html');
     })
 
     $('#dig').click(function () {
@@ -80,7 +75,52 @@ $(document).ready(function () {
     $('#dbba').click(function () {
         $('#page-content-wrapper').load('bba.html');
     })
-   
+
+    $('#xFG').click(function () {
+        $('#page-content-wrapper').load('French-Garden.html');
+    })
+    $('#xRelic-Garden').click(function () {
+        $('#page-content-wrapper').load('Relic-Garden.html');
+    })
+    $('#xNew-Trees').click(function () {
+        $('#page-content-wrapper').load('New-Trees.html');
+    })
+    $('#xCamellias').click(function () {
+        $('#page-content-wrapper').load('Camellias.html');
+    })
+    $('#xDiamond-Jubilee-Copse').click(function () {
+        $('#page-content-wrapper').load('Diamond-Jubilee-Copse.html');
+    })
+    $('#xFriends-Glade').click(function () {
+        $('#page-content-wrapper').load('Friends-Glade.html');
+    })
+    $('#xBarrow-Centre').click(function () {
+        $('#page-content-wrapper').load('Barrow-Centre.html');
+    })
+    $('#xBatcam').click(function () {
+        $('#page-content-wrapper').load('Batcam.html');
+    })
+    $('#xFire-Auxiliary').click(function () {
+        $('#page-content-wrapper').load('Fire-Auxiliary.html');
+    })
+    $('#xDeer-Garden').click(function () {
+        $('#page-content-wrapper').load('Deer-Garden.html');
+    })
+    $('#xKnot-Garden').click(function () {
+        $('#page-content-wrapper').load('Knot-Garden.html');
+    })
+    $('#xCedar-Seat').click(function () {
+        $('#page-content-wrapper').load('Cedar-Seat.html');
+    })
+    $('#xShell-Seat').click(function () {
+        $('#page-content-wrapper').load('Shell-Seat.html');
+    })
+    $('#xLambert-Lodge').click(function () {
+        $('#page-content-wrapper').load('Lambert-Lodge.html');
+    })
+    $('#xChildrens-Play-Area').click(function () {
+        $('#page-content-wrapper').load('Childrens-play-area.html');
+    })
    
 
 
